c6747264c0554fb3ab189e3bb0e0bc708696c280
thellamacoder <scott.s.klein@gmail.com>
2025-02-25 14:42:59 -0500

    added gitignore and modified files

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-import pygame, constants
+import pygame
+from constants import *
 
 def main():
     print(f"Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}\nScreen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Game Loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen,color="black", )
+        pygame.display.flip()
 
 # This is the end of the file
 
