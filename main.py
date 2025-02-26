47f4888e3444039a95acc1caa34e211cdd0b3769
thellamacoder <scott.s.klein@gmail.com>
2025-02-26 06:44:46 -0500

    game clock and fps handled, added class for the sprite shapes

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ from constants import *
 def main():
     print(f"Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    # Initialize Pygame
     pygame.init()
+    # Define the game's screen size
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # Create a clock object to limit the refresh rate for the screen
+    clock = pygame.time.Clock()
+    # Delta time (dt) to hold the change in time
+    dt = 0
+
 
     # Game Loop
     while True:
@@ -14,6 +21,13 @@ def main():
                 return
         pygame.Surface.fill(screen,color="black", )
         pygame.display.flip()
+
+
+        # this is the end of the game loop
+        # the game loop won't exceed the tick time (in this case, the loop will 
+        # wait until 1/60th of a second has passed)
+        clock.tick(60)
+        dt = (clock.get_time()/1000)
 
 # This is the end of the file
 
