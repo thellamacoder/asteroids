1f041f3c5c63bb1410d2d882fc510283d9546ad2
thellamacoder <scott.s.klein@gmail.com>
2025-02-26 14:45:26 -0500

    fixed the circleshape error

--- player.py
+++ player.py
@@ -2,7 +2,7 @@ import pygame
 from constants import *
 from circleshape import CircleShape
 
-class Player(Circleshape):
+class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
