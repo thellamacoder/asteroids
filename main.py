58e0c7abd728841541354284013c887f7e1fd795
thellamacoder <scott.s.klein@gmail.com>
2025-03-06 07:12:58 -0500

    CH3 L6

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def main():
             for shot in shots:
                 if asteroid.collides_with(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         pygame.Surface.fill(screen,color="black", )
 
